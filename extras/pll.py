cece8ff9acaae22f5fd6bec363a101db6ed77b8a
Electronica y Ciencia <electronicayciencia@gmail.com>
2021-11-26 21:06:00 +0100

    Calculate valid PLL frequencies

--- extras/pll.py
+++ extras/pll.py
@@ -14,17 +14,18 @@ CLKOUT_LOWER_FREQ = 3.125
 
 CLKINDIV_VALUES = range(1, 64)
 CLKFBDIV_VALUES = range(1, 64)
-CLKOUTDIV_VALUES = range(2, 128)
+CLKOUTDIV_VALUES = range(2, 130, 2) # must be even
 
 possible_freqs = {}
 
-
+# Test all possible values of input divider
 for clkindiv in CLKINDIV_VALUES:
     clk = CLKIN / clkindiv
     
     if not (CLKINDIV_LOWER_FREQ <= clk <= CLKINDIV_UPPER_FREQ):
         continue
 
+    # Test all possible values of feedback divider
     for clkfbdiv in CLKFBDIV_VALUES:
         clkout = clk * clkfbdiv
 
@@ -34,8 +35,10 @@ for clkindiv in CLKINDIV_VALUES:
         if round(clkout,3) not in possible_freqs:
             possible_freqs[round(clkout,3)] = []
         
-        possible_freqs[round(clkout,3)].append([clkindiv,clkfbdiv,'-'])
+        # These modes do not use OUTD so they come first
+        possible_freqs[round(clkout,3)].insert(0, [clkindiv,clkfbdiv,'-'])
 
+        # Test all possible values of feedback divider
         for clkoutdiv in CLKOUTDIV_VALUES:
             clkoutd = clkout / clkoutdiv
 
