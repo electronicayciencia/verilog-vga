d6b6fa31987e632c491739d5d37d4b4dad0aab7f
Electronica y Ciencia <electronicayciencia@gmail.com>
2021-12-02 23:10:55 +0100

    Reduced image size to fit in memory

---
+++ 2-image/extras/img2mi_mono.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+# Translate monochromatic image to Gowin Memory Initialization file
+
+import sys
+from PIL import Image
+from math import log2, ceil
+
+
+def usage():
+    print("Translate monochromatic image to Gowin Memory Initialization file")
+    print("Usage: {} image_file > file.im".format(sys.argv[0]))
+    exit(1)
+
+def miheader(depth, width):
+    """ Return MI header """
+    miheader = "#File_format=AddrHex\n" \
+             + "#Address_depth={}\n".format(depth) \
+             + "#Data_width={}".format(width)
+
+    return miheader
+
+
+def depth(im):
+    """Return depth and check number of address lines"""
+    (h,v) = im.size
+    addrh = ceil(log2(h-1))
+    addrv = ceil(log2(v-1))
+
+    if (addrh+addrv > 16):
+        print("Error: not enough memory address lines ({}).".format(addrh+addrv))
+        exit(1)
+    
+    return 2**(addrh+addrv)-1
+    
+def convert_mono(im):
+    """ Read image. FIX: Assume 8bit address for cols and lines. """
+
+    print(miheader(depth(im), 1))
+    
+    px = im.load()
+    for lin in range(im.size[1]):
+        for col in range(im.size[0]):
+            if px[col, lin] > 0:
+                print("%02x%02x:%d" % (lin, col, 1))
+    
+
+def main():
+    if len(sys.argv) <= 1:
+        usage()
+
+    im = Image.open(sys.argv[1])
+    convert_mono(im)
+
+
+
+main()
