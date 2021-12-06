7cd8828784b944f9dae5341c0e6e760f68117ddc
Electronica y Ciencia <electronicayciencia@gmail.com>
2021-12-06 21:30:29 +0100

    Simplify delay module.

--- 2-image/src/common/delay.v
+++ 2-image/src/common/delay.v
@@ -1,13 +1,12 @@
+// Delay a signal 1 clock cycle
 module delay (
     input clk,
     input in,
-    output out
+    output reg out
 );
 
-reg [5:0] S;
-assign out = S[0]; // how many cycles - 1?
 
 always @(posedge clk) 
-    S <= {S[4:0], in};
+    out <= in;
 
 endmodule
