9c6bfd9a594d752b72c1c7071d5a4b1848e9e8d3
Electronica y Ciencia <electronicayciencia@gmail.com>
2023-11-05 21:30:41 +0100

    Simpler divider

--- 3-text/src/color_8x16/blinker.v
+++ 3-text/src/color_8x16/blinker.v
@@ -2,16 +2,14 @@
 // VGA blinks every 16 frames (1.8Hz @ 60Hz)
 module blinker (
     input      i_vsync, // frame
-    output reg o_bl     // blinking line
+    output     o_bl     // blinking line
 );
 
-reg [3:0] counter;
+reg [4:0] counter;
 
-always @(negedge i_vsync) begin
+assign o_bl = counter[4];
+
+always @(negedge i_vsync)
     counter <= counter + 1'b1;
-
-    if (counter == 0)
-        o_bl <= ~o_bl;
-end
 
 endmodule
