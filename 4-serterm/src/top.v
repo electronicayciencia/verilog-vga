6d564e0fde6b1e962d8310809eecc94befac67d6
Electronica y Ciencia <electronicayciencia@gmail.com>
2023-11-17 23:37:56 +0100

    UART repo example

--- 4-serterm/src/top.v
+++ 4-serterm/src/top.v
@@ -11,13 +11,17 @@ module top (
 //    output LED_G,
 //    output LED_B,
     input BTN_A,
-    input BTN_B
+    input BTN_B,
+    input wire uart_rxd,
+    output wire uart_txd
 );
 
 localparam false = 1'b0;
 localparam true = 1'b1;
 
 localparam char = 8'h41;
+
+wire rst = false;
 
 wire CLK_12MHZ;
 
@@ -27,8 +31,6 @@ clk_div clk_div (
     .i_factor(5'd0),     // 0: /2,  1: /4,  2: /8 ...
     .o_clk(CLK_12MHZ)
 );
-
-
 
 
 push_button m_BTN_A (
@@ -59,6 +61,78 @@ control control (
     .o_LCD_CLK   (LCD_CLK),
     .o_LCD_DEN   (LCD_DEN)
 );
+
+/**************************/
+/* UART
+/**************************/
+
+
+reg [7:0] uart_tx_axis_tdata;
+reg uart_tx_axis_tvalid;
+wire uart_tx_axis_tready;
+
+wire [7:0] uart_rx_axis_tdata;
+wire uart_rx_axis_tvalid;
+reg uart_rx_axis_tready;
+
+uart
+uart_inst (
+    .clk(XTAL_IN),
+    .rst(rst),
+    // AXI input
+    .s_axis_tdata(uart_tx_axis_tdata),
+    .s_axis_tvalid(uart_tx_axis_tvalid),
+    .s_axis_tready(uart_tx_axis_tready),
+    // AXI output
+    .m_axis_tdata(uart_rx_axis_tdata),
+    .m_axis_tvalid(uart_rx_axis_tvalid),
+    .m_axis_tready(uart_rx_axis_tready),
+    // uart
+    .rxd(uart_rxd),
+    .txd(uart_txd),
+    // status
+    .tx_busy(),
+    .rx_busy(),
+    .rx_overrun_error(),
+    .rx_frame_error(),
+    // configuration
+    .prescale(125000000/(9600*8))
+);
+
+//assign led = sw;
+//assign led = uart_tx_axis_tdata;
+
+always @(posedge XTAL_IN or posedge rst) begin
+    if (rst) begin
+        uart_tx_axis_tdata <= 0;
+        uart_tx_axis_tvalid <= 0;
+        uart_rx_axis_tready <= 0;
+    end else begin
+        if (uart_tx_axis_tvalid) begin
+            // attempting to transmit a byte
+            // so can't receive one at the moment
+            uart_rx_axis_tready <= 0;
+            // if it has been received, then clear the valid flag
+            if (uart_tx_axis_tready) begin
+                uart_tx_axis_tvalid <= 0;
+            end
+        end else begin
+            // ready to receive byte
+            uart_rx_axis_tready <= 1;
+            if (uart_rx_axis_tvalid) begin
+                // got one, so make sure it gets the correct ready signal
+                // (either clear it if it was set or set it if we just got a
+                // byte out of waiting for the transmitter to send one)
+                uart_rx_axis_tready <= ~uart_rx_axis_tready;
+                // send byte back out
+                uart_tx_axis_tdata <= uart_rx_axis_tdata;
+                uart_tx_axis_tvalid <= 1;
+            end
+        end
+    end
+end
+
+
 
 
 endmodule
